1c3ec534d62058a12d96d1908d9288043e406297
Marc Magon <ged_marc@hotmail.com>
2017-09-30 03:31:57 +0200

    Data Tables Updates
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

--- src/za/co/mmagon/jwebswing/plugins/bootstrap/buttons/BSButtonDangerOutline.java
+++ src/za/co/mmagon/jwebswing/plugins/bootstrap/buttons/BSButtonDangerOutline.java
@@ -25,7 +25,7 @@ package za.co.mmagon.jwebswing.plugins.bootstrap.buttons;
  * @version 1.0
  * @since 01 Jan 2017
  */
-public class BSButtonDangerOutline extends BSButton
+public class BSButtonDangerOutline<J extends BSButtonDangerOutline<J>> extends BSButton<J>
 {
 	
 	private static final long serialVersionUID = 1L;
