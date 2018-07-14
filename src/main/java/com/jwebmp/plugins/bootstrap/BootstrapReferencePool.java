5977bb618a02ef260586045148cf127c69d81f09
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:48:28 +0200

    JDK 10 Structure and Configuration

---
+++ src/main/java/com/jwebmp/plugins/bootstrap/BootstrapReferencePool.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jwebmp.plugins.bootstrap;
+
+import com.jwebmp.base.references.CSSReference;
+import com.jwebmp.base.references.JavascriptReference;
+import com.jwebmp.base.servlets.enumarations.RequirementsPriority;
+import com.jwebmp.base.servlets.interfaces.ReferencePool;
+
+/**
+ * @author GedMarc
+ * @since 20 Apr 2016
+ */
+public enum BootstrapReferencePool
+		implements ReferencePool
+{
+	BootstrapCoreReference(
+			new JavascriptReference("BSCore", 3.36, "bower_components/bootstrap/dist/js/bootstrap.min.js", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js",
+			                        10),
+			new CSSReference("BSCore", 3.36, "bower_components/bootstrap/dist/css/bootstrap.min.css", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css", 10)),
+	BootstrapNumberSpinnerReference(new JavascriptReference("NumberSpinnerReference", 3.36, "bs4custom/bsnumberspinner.js")
+			, new CSSReference("NumberSpinnerReference", 3.36, "bs4custom/bsnumberspinner.js"));
+
+	private JavascriptReference javaScriptReference;
+	private CSSReference cssReference;
+
+	BootstrapReferencePool()
+	{
+	}
+
+	BootstrapReferencePool(JavascriptReference javaScriptReference, CSSReference cssReference)
+	{
+		this.javaScriptReference = javaScriptReference;
+		if (this.javaScriptReference != null)
+		{
+			this.javaScriptReference.setPriority(RequirementsPriority.Third);
+		}
+		this.cssReference = cssReference;
+		if (this.cssReference != null)
+		{
+			this.cssReference.setPriority(RequirementsPriority.Third);
+		}
+	}
+
+	@Override
+	public CSSReference getCssReference()
+	{
+		return cssReference;
+	}
+
+	@Override
+	public void setCssReference(CSSReference cssReference)
+	{
+		this.cssReference = cssReference;
+	}
+
+	@Override
+	public JavascriptReference getJavaScriptReference()
+	{
+		return javaScriptReference;
+	}
+
+	@Override
+	public void setJavaScriptReference(JavascriptReference javaScriptReference)
+	{
+		this.javaScriptReference = javaScriptReference;
+	}
+}
