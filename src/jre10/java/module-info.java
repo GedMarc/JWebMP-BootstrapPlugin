1561f1181fc4d9a8161bddbe9c7a3c612f2154da
GedMarc <ged_marc@hotmail.com>
2018-07-26 23:58:56 +0200

    JDK 10 Exposures and operations

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -53,4 +53,47 @@ module com.jwebmp.plugins.bootstrap {
 
 	provides IPageConfigurator with BootstrapPageConfigurator;
 
+	opens com.jwebmp.plugins.bootstrap to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.collapse to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.alerts to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.alerts.events to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.badge to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.breadcrumbs to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.buttons to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.buttons.groups to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.buttons.toolbars to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.carousel to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.carousel.events to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.carousel.features to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.close to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.containers to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.dropdown to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.dropdown.menu to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.forms to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.forms.groups to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.forms.groups.sets to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.forms.controls to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.jumbotron to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.listgroup to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.media to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.modal to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.modal.events to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.modal.features to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.navbar to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.navbar.brand to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.navbar.text to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.navbar.toggler to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.navs to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.options to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.pagination to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.panel to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.popovers to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.progressbar to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.spinner to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.tabs to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.toggle to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.tooltips to com.fasterxml.jackson.databind,com.jwebmp.core;
+	opens com.jwebmp.plugins.bootstrap.progressbar.bar to com.fasterxml.jackson.databind,com.jwebmp.core;
+
+
 }
